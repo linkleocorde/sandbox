1477a2ec5becda4f7a2409a464d4c03e07f45fdb
Link <linkleocorde@users.noreply.github.com>
2018-09-11 14:12:27 -0700

    Fun
    Test

---
+++ counting.ps1
@@ -0,0 +1,25 @@
+﻿Write-Host –NoNewLine "Counting from 1 to 9 (in seconds):  "
+
+foreach($element in 1..3){
+
+  Write-Host –NoNewLine  "${element} " -BackgroundColor "Green" -ForegroundColor "Black"
+
+  Start-Sleep –Seconds 1
+
+}
+
+foreach($element in 4..6){
+
+  Write-Host –NoNewLine  "${element} " -BackgroundColor "Yellow" -ForegroundColor "Black"
+
+  Start-Sleep –Seconds 1
+
+}
+
+foreach($element in 7..9){
+
+  Write-Host –NoNewLine  "${element} " -BackgroundColor "Red" -ForegroundColor "Black"
+
+  Start-Sleep –Seconds 1
+
+}
